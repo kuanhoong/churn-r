37aa62f54a0a22ff97878c509d1116bc41575504
Kuan Hoong <kuanhoong@gmail.com>
2017-01-16 16:15:11 +0800

    churn data

---
+++ customer_churn.R
@@ -0,0 +1,11 @@
+###########################################
+# Churn Analytics using R                 #
+# Poo Kuan Hoong                          #
+# Malaysia R User Group Meetup            #  
+# http://www.github.com/kuanhoong/churn-r #
+###########################################
+
+setwd('C:/Users/Kuan/Documents/github/churn-r')
+cust_data<-read.csv('telco.csv')
+View(cust_data)
+str(cust_data)
