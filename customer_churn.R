9b9fecfcde12ba29992c0f5b5493cb33a3fed7ee
Kuan Hoong <kuanhoong@gmail.com>
2017-01-17 09:24:57 +0800

    customer_churn.R

--- customer_churn.R
+++ customer_churn.R
@@ -6,6 +6,50 @@
 ###########################################
 
 setwd('C:/Users/Kuan/Documents/github/churn-r')
+
+library(caret)
+library(ggplot2)
+
+#load the data
 cust_data<-read.csv('telco.csv')
 View(cust_data)
 str(cust_data)
+
+#data proprocessing
+
+#data exploratory
+
+#Model Building
+#split data to 80-20
+
+#########################################
+#Model 1: Logistic Regression Model     #
+#########################################
+
+#Predictive Performance Measurement
+
+#Odd Ratio
+
+###############################################
+#Model 2: Support Vector Machine (SVM) Model  #
+###############################################
+
+######################################
+#Model 3: Random Forest Model        #
+######################################
+
+#Variable Importance Plot
+
+
+
+
+
+
+
+
+
+
+
+
+
+
