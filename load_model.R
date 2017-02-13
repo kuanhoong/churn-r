e908c34900b6e48154f2fc8387d4549017d40430
Kuan Hoong <kuanhoong@gmail.com>
2017-02-13 15:23:00 +0800

    final updates

--- load_model.R
+++ load_model.R
@@ -30,16 +30,16 @@ cust_data1$Partner <- recode(cust_data1$Partner, "'Yes'=1; 'No'=0")
 cust_data1$Dependents <- recode(cust_data1$Dependents, "'Yes'=1; 'No'=0")
 cust_data1$PhoneService <- recode(cust_data1$PhoneService, "'Yes'=1; 'No'=0")
 cust_data1$MultipleLines <- recode(cust_data1$MultipleLines, "'Yes'=1; 'No'=0;'No phone service'=3")
-cust_data1$InternetService <- recode(cust_data$InternetService, "'No'=0; 'DSL'=1;'Fiber optic'=2")
+cust_data1$InternetService <- recode(cust_data1$InternetService, "'No'=0; 'DSL'=1;'Fiber optic'=2")
 cust_data1$OnlineSecurity <- recode(cust_data1$OnlineSecurity, "'No'=0; 'Yes'=1;'No internet service'=2")
-cust_data1$OnlineBackup <- recode(cust_data$OnlineBackup, "'No'=0; 'Yes'=1;'No internet service'=2")
+cust_data1$OnlineBackup <- recode(cust_data1$OnlineBackup, "'No'=0; 'Yes'=1;'No internet service'=2")
 cust_data1$DeviceProtection <- recode(cust_data1$DeviceProtection, "'No'=0; 'Yes'=1;'No internet service'=2")
 cust_data1$TechSupport <- recode(cust_data1$TechSupport, "'No'=0; 'Yes'=1;'No internet service'=2")
 cust_data1$StreamingTV <- recode(cust_data1$StreamingTV, "'No'=0; 'Yes'=1;'No internet service'=2")
-cust_data1$StreamingMovies <- recode(cust_data$StreamingMovies, "'No'=0; 'Yes'=1;'No internet service'=2")
+cust_data1$StreamingMovies <- recode(cust_data1$StreamingMovies, "'No'=0; 'Yes'=1;'No internet service'=2")
 cust_data1$Contract <- recode(cust_data1$Contract, "'Month-to-month'=0; 'One year'=1;'Two year'=2")
 cust_data1$PaperlessBilling <- recode(cust_data1$PaperlessBilling, "'Yes'=1; 'No'=0")
-cust_data1$PaymentMethod <- recode(cust_data$PaymentMethod, "'Electronic check'=1; 'Mailed check'=2;'Bank transfer (automatic)'=3; 'Credit card (automatic)'=4")
+cust_data1$PaymentMethod <- recode(cust_data1$PaymentMethod, "'Electronic check'=1; 'Mailed check'=2;'Bank transfer (automatic)'=3; 'Credit card (automatic)'=4")
 
 #########################
 # load model            #        
@@ -62,8 +62,10 @@ load('churnmodel.rda')
 #                        + PhoneService
 #                        + Dependents
 
+#for glm, requires to select the same variables used in training
 cust_data1 <- cust_data1[,c("Contract","InternetService","tenure","PaperlessBilling","TotalCharges","MultipleLines","PaymentMethod","SeniorCitizen","StreamingTV","OnlineSecurity","TechSupport","StreamingMovies","MonthlyCharges","PhoneService","Dependents","Churn")]
 
+#churn prediction use glm
 cust_data1$Churn <- predict(logic_reg, cust_data1[, -16], type = 'response')
 
 #set threshold: Greater than 0.5, Churn = YES (1)
@@ -71,3 +73,7 @@ cust_data1[cust_data1$Churn>0.5] <- 1
 cust_data1[cust_data1$Churn<0.5] <- 0
 
 print(cust_data1$Churn)
+
+# Stop the clock
+proc.time() - ptm
+
